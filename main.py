8f5e1386882bd92629225a4aef542d217eb8d6f9
C-NERD <alayaa694@gmail.com>
2022-08-04 21:44:04 +0100

    add tweetvibe endpoint

--- main.py
+++ main.py
@@ -1,17 +1,69 @@
 import dotenv
 from pathlib import Path
-from tweetvibe.utils import datatypes 
+from tweetvibe.utils import datatypes, utils
 from tweetvibe.twitter import twitter
-#from flask import App
+from tweetvibe.detectvibe import detectvibe
+from flask import Flask, request, make_response
 
 env_file = Path(__file__).parent / ".env"
 dotenv.load_dotenv(env_file)
 
-#vibe = twitter.Twitter()
-#replies = vibe.get_replies("https://twitter.com/toluogunlesi/status/1554943412100530177")
-#if replies.status:
+def server():
 
-    #for reply in replies.data:
+    app = Flask(__name__, static_folder = "public/", template_folder = "public/html/")
+    @app.route("/")
+    async def home():
 
-        #print(reply)
+        return ""
+
+    @app.route("/tweetvibe.json", methods=['POST'])
+    async def tweetvibe():
+        
+        LIMIT = 10 ## to minimize polling the google api count will determine the number of replies to be analysed
+        if 'application/json' not in request.headers.get('Content-Type').lower():
+            
+            return dict(datatypes.ErrorData(False, "Content-Type not supported!", None))
+
+        body = request.json
+        tweet_url = body.get("tweet_url")
+        if utils.isemptyorspace(tweet_url):
+
+            return dict(datatypes.ErrorData(False, "tweet_url not found in request body", None))
+
+        replies = twitter.Twitter().get_replies(tweet_url)
+        if not replies.status:
+
+            return dict(replies)
+
+        sentiment_cls = detectvibe.SentimentAnalyser()
+        count = 0
+        result = []
+        for reply in replies.data:
+
+            text = reply.get("text")
+            if utils.isemptyorspace(text):
+
+                continue
+
+            analysis = sentiment_cls.get_text_vibe(text)
+            if not analysis.status:
+
+                continue
+
+            result.append({
+                "reply" : reply,
+                "analysis" : analysis.data
+            })
+            count += 1
+
+            if count == LIMIT:
+
+                break
+
+        response = make_response(dict(datatypes.ErrorData(True, "", result)))
+        return response
+
+    app.run("localhost", 5000, True)
+
+server()
 
