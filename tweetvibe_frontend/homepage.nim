94667a315496d9080c25daa443c7a0251cd314fd
C-NERD <alayaa694@gmail.com>
2022-08-09 21:39:54 +0100

    completed frontend nim code

--- tweetvibe_frontend/homepage.nim
+++ tweetvibe_frontend/homepage.nim
@@ -1,9 +1,20 @@
-import utils, karax / [karax, karaxdsl, vdom], asyncjs
+import utils, karax / [karax, karaxdsl, vdom, vstyles], asyncjs, dom
 #from jsre import newRegExp, contains
 from strutils import isEmptyOrWhitespace, split, contains
+from strformat import fmt
+from sugar import `=>`
 #from tables import getOrDefault
 
-var analysis_node : VNode = buildHtml span(id = "tweet_analysis")
+type
+
+    TweetReplies = ref object of VComponent
+
+        show : bool
+
+var 
+    analysis_node : VNode = buildHtml span(id = "tweet_analysis")
+    tweet_replies : TweetReplies
+    #show_replies : Vstyle = style(display, "none")
 proc isValidTweetUrl(url : string) : bool =
     ## checks if url is a valid tweet url
     
@@ -23,66 +34,260 @@ proc isValidTweetUrl(url : string) : bool =
 
             return partition]#
 
-proc tweet(tweet : Tweet) : VNode =
+proc tweet(tweet : Tweet, vibe : Vibe, main_tweet : bool = false) : VNode =
 
-    result = buildHtml span(class = "tweet-meta"):
+    let vibe_color : Vstyle = block :
 
-        for word in tweet.text.split():
+        var vibe_color : VStyle
+        case vibe
 
-            if '@' in word:
+        of Negative:
 
-                em(class = "coloured-text"):
+            vibe_color = style(backgroundColor, "rgb(211, 24, 63)")
 
-                    text word
+        of Neutral:
+
+            vibe_color = style(backgroundColor, "rgb(24, 63, 211)")
+
+        of Vibe.Positive:
+
+            vibe_color = style(backgroundColor, "rgb(63, 211, 24)")
+
+        vibe_color
+
+    result = buildHtml span(class = "root_tweet"):
+
+        tdiv(class = "vibe_status"):
+
+            tdiv(class = "vibe-color-code", style = vibe_color)
+            if not main_tweet:
+
+                h3:
+
+                    text fmt"Tweet gives {vibe} vibes"
+                
+            else:
+
+                h3:
+
+                    text fmt"Tweet replies are mostly {vibe}"
+
+        span(class = "tweet-meta"):
+
+            text tweet.text
+
+proc replies(tweet_replies : var TweetReplies = tweet_replies, reply_vibes : seq[ReplyVibe]) : TweetReplies =
+
+    proc render(tweet_replies : VComponent) : VNode =
+
+        let self = TweetReplies(tweet_replies)
+        self.markDirty()
+
+        let replies_style : VStyle = block:
+
+            var replies_style : VStyle
+            if self.show:
+
+                replies_style = style(display, "content")
 
             else:
 
-                text word
+                replies_style = style(display, "none")
+
+            replies_style
+
+        result = buildHtml span(id = "tweet_replies", style = replies_style):
+
+            for tweet_data in reply_vibes:
+
+                tweet(tweet_data.reply, tweet_data.analysis.vibe)
+
+    if tweet_replies.isNil():
+
+        tweet_replies = newComponent(TweetReplies, render)
+
+    return tweet_replies
 
 proc analyseTweet(url : string) =
 
-    fetchJsonImpl[ErrorData[VibeAnalysis]]("/tweetvibe.json", ($toJson((tweet_url : url))).cstring, Post, 10000):
+    fetchJsonImpl[ErrorData[VibeAnalysis]]("/tweetvibe.json", ($toJson((tweet_url : url))).cstring, Post, 60000):
+
+        var 
+            total_vibes : tuple[negative, neutral, positive : int]
+            vibe_ratings : seq[cint]
+            tweets, tweet_ids, bar_colors, bar_borders : seq[cstring]
+
+        for each in resp_obj.data.result:
+
+            case each.analysis.vibe
+
+            of Negative:
+
+                total_vibes.negative.inc()
+                bar_colors.add("rgb(211, 24, 63)".cstring)
+
+            of Neutral:
+
+                total_vibes.neutral.inc()
+                bar_colors.add("rgb(24, 63, 211)".cstring)
+
+            of Vibe.Positive:
+
+                total_vibes.positive.inc()
+                bar_colors.add("rgb(63, 211, 24)".cstring)
+
+            bar_borders.add("#1e1f22".cstring)
+            vibe_ratings.add(each.analysis.score.cint)
+            tweets.add(each.reply.text.cstring)
+            tweet_ids.add(each.reply.tweet_id.cstring)
+
+        let tweet_vibe : Vibe = block :
+
+            var tweet_vibe : Vibe 
+            if total_vibes.negative > total_vibes.neutral:
+                
+                if total_vibes.negative > total_vibes.positive:
+
+                    tweet_vibe = Negative
+
+                else:
+
+                    tweet_vibe = Vibe.Positive
+            
+            elif total_vibes.neutral > total_vibes.negative:
+
+                if total_vibes.neutral > total_vibes.positive:
+
+                    tweet_vibe = Neutral
+                
+                else:
+
+                    tweet_vibe = Vibe.Positive
+
+            elif total_vibes.positive > total_vibes.negative:
+
+                if total_vibes.positive > total_vibes.neutral:
+
+                    tweet_vibe = Vibe.Positive
+
+                else:
+
+                    tweet_vibe = Neutral
+
+            elif total_vibes.negative == total_vibes.neutral:
+
+                if total_vibes.positive > 0:
+
+                    tweet_vibe = Neutral
+
+                else:
+
+                    tweet_vibe = Negative
+
+            elif total_vibes.negative == total_vibes.positive:
+
+                if total_vibes.neutral > 0:
+
+                    tweet_vibe = Neutral
+
+                else:
+
+                    tweet_vibe = Negative
+
+            elif total_vibes.neutral == total_vibes.positive:
+
+                if total_vibes.negative > 0:
+
+                    tweet_vibe = Neutral
+
+                else:
+
+                    tweet_vibe = Vibe.Positive
+
+            tweet_vibe
 
         analysis_node = buildHtml span(id = "tweet_analysis"): 
-            
-            span:
 
-                tweet(resp_obj.data)
+            tweet(resp_obj.data, tweet_vibe, true)
 
-                span:
+            span(id = "analysis-charts"):
 
-                    span:
-
-                        h3:
-                            text "% vibe rating"
-                        canvas(id = "vibe-mean-rating")
-
-                    span:
-
-                        h3:
-                            text "Mean vibe rating"
-                        em(id = "Tweet-mean-vibe")
-
-            span(id = "replies-charts"):
-
-                span:
+                span(id = "replies-scores-container"):
 
                     h3:
-                        text "Vibe rating (0-9)"
-                    canvas(id = "replies-bar-chart")
+                        text "Replies scores"
+                    canvas(id = "replies-scores")
 
-                span:
+                span(id = "vibe-ratings-container"):
 
                     h3:
-                        text "Vibe rating (-ve, ve, +ve)"
-                    canvas(id = "replies-vibe-bar-char")
+                        text "Vibe ratings"
+                    canvas(id = "vibe-ratings")
+
+            span(id = "reply-controller"):
+
+                text "Replies"
+                tdiv(id = "drop-btn"):
+
+                    proc onclick() {.closure.} =
+
+                        tweet_replies.markDirty()
+                        if tweet_replies.show:
+
+                            tweet_replies.show = false
+                        
+                        else:
+                            
+                            tweet_replies.show = true
+                            
+            replies(reply_vibes = resp_obj.data.result)
+
+        proc display_charts() {.closure.} =
+
+            setForeignNodeId("vibe-ratings-container")
+            setForeignNodeId("replies-scores-container")
+
+            showDoughnutChart(
+                "vibe-ratings".cstring, "Mean vibe rating".cstring,
+                @[total_vibes.negative.cint, total_vibes.neutral.cint, total_vibes.positive.cint],
+                @["Negative".cstring, "Neutral".cstring, "Positive".cstring],
+                @[
+                    "rgb(211, 24, 63)".cstring,
+                    "rgb(24, 63, 211)".cstring,
+                    "rgb(63, 211, 24)".cstring
+                ],
+                @[
+                    "#1e1f22".cstring,
+                    "#1e1f22".cstring,
+                    "#1e1f22".cstring
+                ]
+            )
+
+            showBarChart(
+                "replies-scores".cstring, "Replies vibe ratings".cstring,
+                vibe_ratings, tweet_ids,
+                bar_colors, bar_borders
+            )
+
+        show_loading_screen = false
+        onfetch_complete = () => (
+            var interval : Interval;
+            interval = window.setInterval(
+                () => (
+                    if not getElementById("vibe-ratings").isNil() and not getElementById("replies-scores").isNil():
+
+                        display_charts();
+                        interval.clearInterval();
+                ), 50
+            );
+        )
 
 proc homepage() : VNode =
 
     result = buildHtml main():
-
+        
         navbar()
-
+        
         main(id = "mainbody"):
 
             span(id = "contentbody"):
@@ -105,6 +310,9 @@ proc homepage() : VNode =
 
                                 showToast("Cannot get tweet id from tweet url")
                                 return]#
+                            
+                            show_loading_screen = true
+                            redraw()
 
                             analyseTweet(tweet_url)
 
@@ -117,14 +325,13 @@ proc homepage() : VNode =
         footbar()
         overlay()
         toast()
+        loadingScreen()
 
-proc callback() =
+#[proc callback() =
 
-    #[let tweet_id = url_query.getOrDefault("tweet_id")
+    let tweet_id = url_query.getOrDefault("tweet_id")
     if not tweet_id.isEmptyOrWhitespace():
 
         analyseTweet(tweet_id)]#
-    
-    discard
 
-setRenderer homepage, "ROOT", callback
+setRenderer homepage, "ROOT"
