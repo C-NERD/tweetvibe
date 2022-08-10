92b294df2f35aa676819b3fbf9431eac44d39b85
C-NERD <alayaa694@gmail.com>
2022-08-10 21:36:32 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@
 ## for debian and arch linux only
 ## make sure to run with root permission
 
-from platform import python_version_tuple, system
 from subprocess import run
-from re import search
 from os import geteuid
 import logging
 
@@ -60,59 +58,16 @@ if __name__ == "__main__":
         logging.info("quiting...")
         quit(-1)
 
-    logging.info("checking python version...")
-    ## check if python version is less than 3.7, if so update python
-    py_ver = python_version_tuple()
-    update_py = False
-    if int(py_ver[0]) < 3:
-
-        update_py = True
-    
-    elif int(py_ver[1]) < 7:
-
-        update_py = True
-
-    if update_py:
-
-        logging.info("updating to new version of python...")
-        run(["sudo", pkg_manager, install_cmd, "python3"])
+    logging.info("updating to new version of python...")
+    run(["sudo", pkg_manager, install_cmd, "python3"])
     
     logging.info("installing pip...")
     run(["sudo", pkg_manager, install_cmd, "pip"])
 
-    logging.info("checking nim version...")
-    ## check if nim version is less than 1.4.0 if so update nim
-    update_nim = False
-    try:
-
-        nim_ver_info = run(["nim", "-v"], text = True, capture_output = True)
-        if nim_ver_info == None:
-
-            update_nim = True
-        
-        else:
-
-            nim_ver = search(r"[0-9].[0-9].[0-9]", nim_ver_info.stdout)
-            nim_ver_splited = nim_ver[0].split(".")
-
-            if int(nim_ver_splited[0]) < 1:
-
-                update_nim = True
-        
-            elif int(nim_ver_splited[1]) < 4:
-
-                update_nim = True
-
-    except FileNotFoundError:
-
-        update_nim = True
-
-    if update_nim:
-
-        logging.info("updating to new version of nim...")
-        run(["sudo", pkg_manager, install_cmd, "nim"])
-        run(["nimble", "install", "choosenim"])
-        run(["choosenim", "update", "1.6.6"])
+    logging.info("updating to new version of nim...")
+    run(["sudo", pkg_manager, install_cmd, "nim"])
+    run(["nimble", "install", "choosenim"])
+    run(["choosenim", "update", "1.6.6"])
 
     logging.info("installing package dependencies...")
     ## install package dependencies
